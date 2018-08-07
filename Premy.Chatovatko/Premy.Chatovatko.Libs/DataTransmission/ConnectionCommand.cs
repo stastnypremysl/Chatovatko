6e56bfcf21ffc4a0079cdcbd576ffb66323c9d5e
Přemysl Šťastný <premysl.stastny@hotmail.com>
2018-08-07 21:21:08 +0200

    Changed connection constants

--- Premy.Chatovatko/Premy.Chatovatko.Libs/DataTransmission/ConnectionCommand.cs
+++ Premy.Chatovatko/Premy.Chatovatko.Libs/DataTransmission/ConnectionCommand.cs
@@ -6,16 +6,19 @@ namespace Premy.Chatovatko.Libs.DataTransmission
 {
     public enum ConnectionCommand
     {
+
+        END_CONNECTION = 0,
+
         /// <summary>
         /// Client wants to delete contact from Trusted Contacts.
         /// No AES key will be deleted.
         /// </summary>
-        UNTRUST_CONTACT = 0,
+        UNTRUST_CONTACT = 1,
         /// <summary>
         /// The client wants to add a contact to Trusted Contacts. 
         /// If no AES key has been created already, it will be created.
         /// </summary>
-        TRUST_CONTACT = 1,
+        TRUST_CONTACT = 2,
 
         /// <summary>
         /// Client wants to push.
@@ -24,8 +27,6 @@ namespace Premy.Chatovatko.Libs.DataTransmission
         /// <summary>
         /// Client wants to pull.
         /// </summary>
-        PULL = 6,
-
-        END_CONNECTION = 10
+        PULL = 6
     }
 }
