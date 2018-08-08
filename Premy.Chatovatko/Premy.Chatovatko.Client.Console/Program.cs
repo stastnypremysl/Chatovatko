bed73b034e70d6f4d280d6eb083edf683d9a26c8
Přemysl Šťastný <premysl.stastny@hotmail.com>
2018-08-08 19:15:49 +0200

    Changed format of ls

--- Premy.Chatovatko/Premy.Chatovatko.Client.Console/Program.cs
+++ Premy.Chatovatko/Premy.Chatovatko.Client.Console/Program.cs
@@ -304,11 +304,11 @@ namespace Premy.Chatovatko.Client
 
         static void WriteUsers()
         {
-            String format = "{0,4} {1,12} {2,12} {3,12} {4,12} {5,30}";
+            String format = "{0,-4} {1,-12} {2,-12} {3,-12} {4,-12} {5,-30}";
             using (Context context = new Context(config))
             {
                 WriteLine();
-                WriteLine("{0,-4} {1,-12} {2,-12} {3,-12} {4,-12} {5,-30}", "Id", "NickName", "Trusted", "AlarmPer", "ContactPer", "UserName");
+                WriteLine(format, "Id", "NickName", "Trusted", "AlarmPer", "ContactPer", "UserName");
                 foreach(var user in 
                     from contacts in context.Contacts
                     join detail in context.ContactsDetail on contacts.PublicId equals detail.ContactId into jDetail
