f3a11b1302480753901d61685dc7663ac911c9d4
Přemysl Šťastný <premysl.stastny@hotmail.com>
2018-08-10 14:20:39 +0200

    Added schema exports.

--- Premy.Chatovatko/Premy.Chatovatko.Libs/DataTransmission/TcpConstants.cs
+++ Premy.Chatovatko/Premy.Chatovatko.Libs/DataTransmission/TcpConstants.cs
@@ -8,6 +8,7 @@ namespace Premy.Chatovatko.Libs.DataTransmission
     {
         public readonly static int MAIN_SERVER_PORT = 8471;
         public readonly static int INFO_SERVER_PORT = 8470;
+        public readonly static int ONLIVE_SERVER_PORT = 8472;
 
         public readonly static int HANDSHAKE_LENGHT = 32;
     }
